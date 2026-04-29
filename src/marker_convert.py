19ce1851608a7e9bf8ae83e76f0cf4cb4ea07ec7
Gustav Idun Sloth <au753860@uni.au.dk>
2026-04-29 13:30:25 +0200

    disable image extraction marker

--- src/marker_convert.py
+++ src/marker_convert.py
@@ -3,6 +3,6 @@ from config import RAW_OUT, EXTRACT_OUT, NUM_THREADS, MARKER_WORKERS
 
 def marker_convert():
     subprocess.run(
-        ["marker", RAW_OUT, "--output_dir", EXTRACT_OUT, "--output_format", "markdown", "--workers", str(MARKER_WORKERS)],
+        ["marker", RAW_OUT, "--output_dir", EXTRACT_OUT, "--output_format", "markdown", "--workers", str(MARKER_WORKERS), "--disable_image_extraction"],
         check=True
     )
